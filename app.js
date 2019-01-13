f9338b0cce3d15ed5547f1730f6aeb082b252ccd
raviguru123 <ravi.guru@paytm.com>
2019-01-13 15:37:48 +0530

    ack

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var
 	express = require("express"),
 	app 	= express(),
 	cors    = require('cors'),
+	_		= require('lodash'),
+	UTIL    = require('util'),
 	port    = process.env.port || 3000,
 	xmlparser  = require('express-xml-bodyparser');
 
@@ -23,9 +25,28 @@ app.all("*", function(request, response) {
 	console.log("starttime",new Date());
 	console.log("<<============= request received from outside=============>>");
 	console.log("body::", request.rawBody);
-	response.status(200).send("\n<============ok its working===========>\n");
+	ack(request, response);
+	//response.status(200).send("\n<============ok its working===========>\n");
 });
 
+
+function ack(request, response) {
+	    var
+        self                = this,
+        refId               = _.get(request,'body.ns2:billfetchrequest.head.0.$.refId',''),
+        msgId               = _.get(request,'body.ns2:billfetchrequest.txn.0.$.msgId','');
+		ts_request          = _.get(request,'body.ns2:billfetchrequest.txn.0.$.ts','');
+		console.log("*************************************");
+		console.log("refId,msgId,ts_request",refId,msgId,ts_request);
+		var str = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ns2:Ack xmlns:ns2="http://bbps.org/schema" api="FETCH_REQUEST" refId="13012019153244D2FGQAVMZG17Y145PFKYB" msgId="130120191532443D7PBWSTG4S7JF6AXMN76" RspCd="Successful" ts="2019-01-13T15:32:46+05:30"/>'
+		
+		var resp = UTIL.format('<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ns2:Ack xmlns:ns2="http://bbps.org/schema" api="FETCH_REQUEST" refId="%s" msgId="%s" RspCd="Successful" ts="%s"/>',
+			refId,
+			msgId,
+			ts_request
+			);
+		response.status(200).send(resp);
+}
 
 
 
@@ -34,4 +55,4 @@ app.listen(port, function(err) {
 		throw err;
 	}
 	console.log("server is listen in port::"+port);
-})
+});
